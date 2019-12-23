14a28763e5bc211a01eb87235dcc28994b79f460
ericksen-github <ericksen.github@gmail.com>
2019-12-23 19:55:00 +0100

    Fixed a decimal bug. Added new/clearer comments. Condensed operator checks.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// a check to reset numbers if a calculation was just completed
 let equalsPressed = false; 
 
 function addListeners() {
@@ -32,17 +33,10 @@ function setKey(e) {
 
 // runs through on "=" key to make sure operators are in display before sortOperations
 function checkForOperators(equation) {
-    let opCheck = false; 
-    for (let i = 0; i < equation.length; i++) {
-        if (isNaN(equation.charAt(i)) && equation.charAt(i) != " " && equation.charAt(i) != "."){
-            opCheck = true; 
-            break; 
-        }
+    if (equation.includes("÷") || equation.includes("*") ||
+        equation.includes("+") || equation.includes("-")) {
+        sortOperations(equation.split(" ")); 
     }
-    if (opCheck == false) {
-        return; 
-    }
-    sortOperations(equation.split(" ")); 
 }
 
 function updateDisplay(key) {
@@ -80,12 +74,12 @@ function clearOrDeleteDisplay(key, equation) {
         return true; 
     } else if (key == "Del") {	
         if (equation.length == 1 || equation == "Cannot divide by 0" || equation == "Infinity") {
-            equation = 0; // checks if display is 1 number or error, then sets to 0
+            equation = 0; // if display is 1 number or error, then sets to 0
         } else if (equation[equation.length-1] == " "){
-            // checks if last of display is operator and removes it and spaces
+            // if last of display is operator and removes it and spaces
             equation = equation.slice(0, -3);
         } else {
-            // removes a single digit from end
+            // removes a single digit/decimal from end
             equation = equation.slice(0, -1); 
         }
         document.getElementById("displayText").innerHTML = equation; 
@@ -95,58 +89,60 @@ function clearOrDeleteDisplay(key, equation) {
 
 // checks for decimals between end of equation and the last operator input
 function checkDecimal(key, equation) {
-    let len = equation.length; 
     let sliced = ""; 
 
-    if (equation.includes(" ")) {
-        for (i = len - 1; i >= 0; i--) {
-            if (equation[i] == " ") {
-                sliced = equation.slice(i); 
+    if (equation.includes(" ")) {                       // if there is an operator, checks for
+        for (i = equation.length - 1; i >= 0; i--) {    // decimal from last operator to
+            if (equation[i] == " ") {                   // end of string and then extracts
+                sliced = equation.slice(i);             // that portion
                 break; 
             }
         }
-    }
-    if (sliced.includes(".")) {
+        if (sliced.includes(".")) { // returns if decimal is in sliced portion
+            return;
+        }
+    } else if (equation.includes(".")) { // only triggers if equation did not have an operator
         return; 
     }
-    if (equalsPressed == true) {
+
+    // checks for recent calc and clears old result instead of adding decimal to end of it
+    if (equalsPressed == true) {  
         document.getElementById("displayText").innerHTML = "0" + key;  
         equalsPressed = false;  
     } else {
-        if (equation[equation.length -1] == " ") {
+        if (equation[equation.length -1] == " ") { // if decimal after operator, also adds 0
             document.getElementById("displayText").innerHTML = equation + "0" + key;
         } else {
             document.getElementById("displayText").innerHTML = equation + key; 
         }
-         
     }
 }
 
-function sortOperations(text) {
+function sortOperations(equation) {
     let temp = []; 
     let next = []; 
     let num;  
 
     // iterates through each in array checking for * or ÷
     // if it finds one, it passes nums next to operator to do math
-    // then pushes new number to array in place of first number and skips over pushing 
+    // then pushes new number to array in place of first number and skips over that
     // operator and other digit. if no * or ÷ is found, pushes everything to temp
-    for (let i = 0; i < text.length; i++) {
-        if (text[i] == "*" || text[i] == "÷") {
-            num = doMath(text[i-1], text[i], text[i+1]);
+    for (let i = 0; i < equation.length; i++) {
+        if (equation[i] == "*" || equation[i] == "÷") {
+            num = doMath(equation[i-1], equation[i], equation[i+1]);
             temp[i-1] = num; 
-            if (text.length > 3) {
-                for (let j = i + 2; j < text.length; j++) {
-                temp.push(text[j]); 
+            if (equation.length > 3) {
+                for (let j = i + 2; j < equation.length; j++) {
+                temp.push(equation[j]); 
                 }
             }
             break; 
         }
-        temp.push(text[i]); 
+        temp.push(equation[i]); 
     }
 
-    // after all * and ÷ have been calculated, does + and -. 
-    // compares array at end of previous and moves to new array
+    // after all * and ÷ have been calculated, looks for + and -. 
+    // takes array temp from previous loop and pushes to array next
     if (!temp.includes("÷") && !temp.includes("*")){
         for (let i = 0; i < temp.length; i++) {
             if (temp[i] == "+" || temp[i] == "-") {
@@ -163,7 +159,7 @@ function sortOperations(text) {
         }
     }
 
-    // if next = 1, should move straight to pushing to display, otherwise
+    // if next = 1, moves straight to pushing to display, otherwise
     // calls sortOperations again depending on what array was left off 
     if (temp.length > 1 && next.length != 1) {
         if (next.length > 1) {
@@ -198,5 +194,4 @@ function doMath(aNum, operator, bNum) {
     }
 }
 
-// initialized events for click/keydown
-addListeners();
+addListeners(); // initialized events for click/keydown
